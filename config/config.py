58b11f9500bf6a957f3d95fa94d70d7bfd765d87
Silenthrax <210396117+Silenthrax@users.noreply.github.com>
2025-05-30 08:12:43 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -13,15 +13,15 @@ BOT_TOKEN = getenv("7882598019:AAF9FW-MiuOZi-VJK41YOjBg44Nrnnnxyw4", None)
 STRING1 = getenv("BQGiH74AMnVmD7Z8p4bdNjNjH0dDq0QBLvwYKMP0qkiIvwjeaBL0sFBVLnGdeTsVJ5ceI00NBOFOSiQEJZTBfVP7iIduh5LPUwQ6XupuaslRZ1H0pC_E2VvbctP27g4ArQ_M4RNA99YBesgCmlg11NpOqVIE8LY3HawAmzKQUbiKtJzRyMfhGUv_cJDnc9dJSL1E8kFn_4Pi0IbEaExKXDU2wxbGZJWtWPr4wQH8-hM-CnN9b1OqBcPYuEQpZNhY2hIOKr8L4IJiOA5eaVhQFqHYWK6Su1MiGQXDEQS85xgB4HQp-hQYeGMqqKdAptr2Nv5iCIVR7_Vofp30SdqCKh1Fm8WD0wAAAAG_nNwGAA", None)
 DB_NAME = "shizuDB"
 MONGO_URL = getenv("mongodb+srv://sr7blackbirds:5mRE2CGlkCXmI5pL@cluster0.jsssmx8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
-OWNER_ID = int(getenv("OWNER_ID", "1716902346"))
-BOT_ID = int(getenv("BOT_ID", "7882598019"))
-SUPPORT_GRP = "bindaas_buddies"
-UPDATE_CHNL = "SR7_BLACKBIRD"
-OWNER_USERNAME = "UrStaRk"
+OWNER_ID = int(getenv("OWNER_ID", "7978151350"))
+BOT_ID = int(getenv("BOT_ID", "7940019678"))
+SUPPORT_GRP = "BESTIE_UNITE_CLUB"
+UPDATE_CHNL = "Silenthrex"
+OWNER_USERNAME = "silenthrax"
 TIME_ZONE = "Asia/Kolkata"
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002540021894"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002267586320"))
 # --------------------------------------------------------------
-SUDOERS = list(map(int, getenv("SUDOERS", "1716902346").split()))
+SUDOERS = list(map(int, getenv("SUDOERS", "7982034286").split()))
 # --------------------------------------------------------------
 
 ### DONT TOUCH or EDIT codes after this line
@@ -30,6 +30,6 @@ BANNED_USERS = filters.user()
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Badhacker98/ShizuChat_Bot",
+    "https://github.com/Silenthrax/AI2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
