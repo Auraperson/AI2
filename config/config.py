2b9cf883ec3c05dc4cf9445c6e154d923bfdd276
Satyam Yadav <githubbysatyamaura@gmail.com>
2025-07-10 12:45:33 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -5,23 +5,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "20661683"
+API_ID = "22727413"
 # -------------------------------------------------------------
-API_HASH = "596794978e0a1b5cb03e76243de90e80"
+API_HASH = "61c37838e6e3193388c7c2fe3e7c8858"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
-DB_NAME = "deepak-bot"
+DB_NAME = "samira-bot"
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7978151350"))
-BOT_ID = int(getenv("BOT_ID", "7940019678"))
+OWNER_ID = int(getenv("OWNER_ID", "7921133034"))
+BOT_ID = int(getenv("BOT_ID", "7791021817"))
 SUPPORT_GRP = "BESTIE_UNITE_CLUB"
 UPDATE_CHNL = "Silenthrex"
 OWNER_USERNAME = "silenthrax"
 TIME_ZONE = "Asia/Kolkata"
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002267586320"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002754356584"))
 # --------------------------------------------------------------
-SUDOERS = list(map(int, getenv("SUDOERS", "7982034286").split()))
+SUDOERS = list(map(int, getenv("SUDOERS", "6766551134").split()))
 # --------------------------------------------------------------
 
 ### DONT TOUCH or EDIT codes after this line
@@ -30,6 +30,6 @@ BANNED_USERS = filters.user()
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Silenthrax/AI2",
+    "https://github.com/Auraperson/AI2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
